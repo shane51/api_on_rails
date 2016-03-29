748ede568230d045e5c0128557ce0c2bf4e621e7
shane51 <suse51@163.com>
2016-03-29 14:35:37 +0800

    Adds show actions fpr users controller

--- market_place_api/config/routes.rb
+++ market_place_api/config/routes.rb
@@ -3,9 +3,9 @@ MarketPlaceApi::Application.routes.draw do
   devise_for :users
   # Api definition
   namespace :api, defaults: { format: :json },constraints: { subdomain: 'api' }, path: '/'  do
-    # We are going to list our resources here
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-
+      # We are going to list our resources here
+      resources :users, :only => [:show]
     end
   end
 end
--- market_place_api/spec/spec_helper.rb
+++ market_place_api/spec/spec_helper.rb
@@ -58,6 +58,16 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/v/3-0/docs
   config.infer_spec_type_from_file_location!
-  config.include(Shoulda::Matchers::ActiveModel, type: :model)
-  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+  # config.include(Shoulda::Matchers::ActiveModel, type: :model)
+  # config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    # Choose a test framework:
+    with.test_framework :rspec
+
+    # Or, choose the following (which implies all of the above):
+    with.library :rails
+  end
 end
