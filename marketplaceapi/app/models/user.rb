e371a0b3f8c5215eb8ae776ca1743233ce9d7226
shane51 <suse51@163.com>
2016-04-05 09:18:51 +0800

    Finished modeling product model along with user associations

--- marketplaceapi/app/models/user.rb
+++ marketplaceapi/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   validates :auth_token, uniqueness: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  has_many :products, dependent: :destroy
   before_create :generate_authentication_token!
   def generate_authentication_token!
     begin
