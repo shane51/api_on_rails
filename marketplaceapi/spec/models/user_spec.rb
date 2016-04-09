7fd77aae035a5a791644840d80fef410584d0454
shane51 <suse51@163.com>
2016-04-09 18:01:17 +0800

    Fix case sensitive issue for shoulda-match for validate_uniqueness_of

--- marketplaceapi/spec/models/user_spec.rb
+++ marketplaceapi/spec/models/user_spec.rb
@@ -6,7 +6,7 @@ describe User do
   subject { @user }
 
   it { should validate_presence_of(:email) }
-  it { should validate_uniqueness_of(:email) }
+  it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
   it { should validate_confirmation_of(:password) }
   it { should allow_value('example@domain.com').for(:email) }
   # we test the user actually respond to this attribute
