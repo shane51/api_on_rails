c306aecd38123a0361b4250436681c53f9c6989c
shane51 <suse51@163.com>
2016-04-11 12:59:37 +0800

    Embeds the products_ids into the user serializera and fetches the correct product from the index action endpoint

--- marketplaceapi/spec/controllers/api/v1/products_controller_spec.rb
+++ marketplaceapi/spec/controllers/api/v1/products_controller_spec.rb
@@ -25,19 +25,35 @@ describe Api::V1::ProductsController do
       4.times { FactoryGirl.create :product }
       get :index
     end
+    context "when is not receiving any product_ids parameter" do
+      it  "returns 4 records from the database" do
+        products_response = json_response
+        expect(products_response[:products].size).to eql 4
+      end
 
-    it  "returns 4 records from the database" do
-      products_response = json_response
-      expect(products_response[:products].size).to eql 4
+      it "returns the user object into each product" do
+        products_response = json_response[:products]
+        products_response.each do |product_response|
+            expect(product_response[:user]).to be_present
+        end
+      end
+      it { should respond_with 200 }
     end
 
-    it "returns the user object into each product" do
-      products_response = json_response[:products]
-      products_response.each do |product_response|
-          expect(product_response[:user]).to be_present
+    context "when is not receiving any product_ids parameter" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        3.times { FactoryGirl.create :product, user: @user }
+        get :index, product_ids: @user.product_ids
+      end
+
+      it "returns just the products that belong to the user" do
+        products_response = json_response[:products]
+        products_response.each do |product_response|
+            expect(product_response[:user][:email]).to eql @user.email
+        end
       end
     end
-    it { should respond_with 200 }
   end
 
   describe "POST #create" do
